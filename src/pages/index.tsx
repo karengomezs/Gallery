6a927fb6278c50ae3af94b25425c72b82bc9b9b9
karengomezs <karengomezsoto1@gmail.com>
2023-04-15 11:43:05 -0500

    add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,31 +34,37 @@ export default function Home(props: Props) {
 
   return (
     <main className="flex min-h-screen flex-col items-center px-20 pb-10 bg-slate-200">
-      <nav className="bg-gray-900 w-screen px-40 py-5 mb-10 flex gap-4">
-        <input
-          onChange={(e) => {
-            setQuerySearchedImage(e?.target?.value);
-          }}
-          className=" h-9 rounded bg-slate-100 border-slate-700 border-2 px-2"
-          type="text"
-          placeholder="Search Image"
-        />
+      <nav className="bg-gray-900 w-screen px-40 py-5 mb-10 flex gap-4 justify-between">
+        <div className="flex gap-3">
+          <input
+            onChange={(e) => {
+              setQuerySearchedImage(e?.target?.value);
+            }}
+            className=" h-9 rounded bg-slate-100 border-slate-700 border-2 px-2"
+            type="text"
+            placeholder="Search Image"
+          />
 
-        <Button
-          name="Search"
-          onclick={() => {
-            if (querySearchedImage) {
-              getSearchedImages(querySearchedImage).then((data) => {
-                setImages(data?.results);
-                setCounterPage(1);
-              });
-            }
-          }}
-        />
+          <Button
+            name="Search"
+            onclick={() => {
+              if (querySearchedImage) {
+                getSearchedImages(querySearchedImage).then((data) => {
+                  setImages(data?.results);
+                  setCounterPage(1);
+                });
+              }
+            }}
+          />
 
-        <Link href="/random">
-          <Button name="Random Photo" />
-        </Link>
+          <Link href="/random">
+            <Button name="Random Photo" />
+          </Link>
+        </div>
+
+        <div className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-yellow-400 font-mono font-semibold">
+          <h1>FastGallery</h1>
+        </div>
       </nav>
 
       <div className="flex flex-wrap justify-center gap-3">{imagesRender}</div>
