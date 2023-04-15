ed411ff5621f0e09c4abde8103219d7c069c31a8
karengomezs <karengomezsoto1@gmail.com>
2023-04-15 11:45:36 -0500

    font buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export function Button({ name, onclick }: Props) {
   return (
     <button
       onClick={onclick}
-      className="rounded bg-slate-300 border-slate-500 border-2 rounded-md text-slate-950  px-2 h-9"
+      className="rounded bg-slate-300 border-slate-500 text-slate-950 font-semibold border-2 rounded-md   px-2 h-9"
     >
       {name}
     </button>
